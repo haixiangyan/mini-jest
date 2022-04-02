7abc7e6eadf4ea8601e23ea688cd49ef025efc3d
haixiangyan <Qq680321>
2022-04-02 13:24:32 +0800

    feat: 获取所有测试文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import glob from 'glob'
+import HasteMap from 'jest-haste-map';
+import { cpus } from 'os';
+import * as path from "path";
 
-const allFiles = glob.sync('**/*.test.js')
+const start = async () => {
+  const root = path.join(__dirname, '../tests');
+  const hasteMap = new HasteMap({
+    rootDir: root, // 主目录
+    roots: [root], // 子目录
+    maxWorkers: cpus().length,
+    platforms: [],
+    extensions: ['js'],
+    name: 'mini-jest'
+  })
 
-console.log(allFiles);
+  const { hasteFS } = await hasteMap.build()
+
+  const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js'], root);
+
+  console.log(testFiles);
+}
+
+start().then();
